871a573340079ba5ce52685f25768f34eaa3c177
masao <sales-info@classcat.com>
2023-09-23 19:55:05 +0900

    localization

--- server/utils/AiProviders/openAi/index.js
+++ server/utils/AiProviders/openAi/index.js
@@ -1,17 +1,42 @@
-class OpenAi {
-  constructor() {
-    const { Configuration, OpenAIApi } = require("openai");
-    const config = new Configuration({
-      apiKey: process.env.OPEN_AI_KEY,
-    });
-    const openai = new OpenAIApi(config);
-    this.openai = openai;
-  }
 
-  isValidChatModel(modelName = "") {
-    const validModels = ["gpt-4", "gpt-3.5-turbo"];
-    return validModels.includes(modelName);
-  }
+const {format} = require('util')
+
+/* ref.
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "",
+  temperature: 0.7,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+});
+*/
+
+class OpenAi {
+
+	// API キーの保持はしていない。
+	constructor() {
+    	const { Configuration, OpenAIApi } = require("openai");
+    	const config = new Configuration({
+      		apiKey: process.env.OPEN_AI_KEY,
+    	});
+    	const openai = new OpenAIApi(config);
+    	this.openai = openai;
+  	}
+
+	//  "gpt-4", "gpt-3.5-turbo" のみ許可。より長いモデルは？
+  	isValidChatModel(modelName = "") {
+    	const validModels = ["gpt-4", "gpt-3.5-turbo"];
+    	return validModels.includes(modelName);
+  	}
 
   async isSafe(input = "") {
     const { flagged = false, categories = {} } = await this.openai
@@ -46,52 +71,58 @@ class OpenAi {
   }
 
   async sendChat(chatHistory = [], prompt, workspace = {}) {
-    const model = process.env.OPEN_MODEL_PREF;
+	console.log("debug >>> IN : sendChat")
+	const model = process.env.OPEN_MODEL_PREF;
     if (!this.isValidChatModel(model))
-      throw new Error(
-        `OpenAI chat: ${model} is not valid for chat completion!`
-      );
+      	throw new Error(
+        		`OpenAI chat: ${model} is not valid for chat completion!`
+    	);
 
+	// max token おいたほうが良いか？
     const textResponse = await this.openai
-      .createChatCompletion({
-        model,
-        temperature: Number(workspace?.openAiTemp ?? 0.7),
-        n: 1,
-        messages: [
-          { role: "system", content: "" },
-          ...chatHistory,
-          { role: "user", content: prompt },
-        ],
-      })
-      .then((json) => {
-        const res = json.data;
-        if (!res.hasOwnProperty("choices"))
-          throw new Error("OpenAI chat: No results!");
-        if (res.choices.length === 0)
-          throw new Error("OpenAI chat: No results length!");
-        return res.choices[0].message.content;
-      })
-      .catch((error) => {
-        console.log(error);
-        throw new Error(
-          `OpenAI::createChatCompletion failed with: ${error.message}`
-        );
-      });
+      	.createChatCompletion({
+        	model,
+        	temperature: 0.5, // Number(workspace?.openAiTemp ?? 0.7),
+        	n: 1,
+			max_tokens: 1000,
+        	messages: [
+          		{ role: "system", content: "" },
+          		...chatHistory,
+          		{ role: "user", content: prompt },
+        	],
+      		})
+      	.then((json) => {
+        	const res = json.data;
+        	if (!res.hasOwnProperty("choices"))
+        		throw new Error("OpenAI chat: No results!");
+        	if (res.choices.length === 0)
+          		throw new Error("OpenAI chat: No results length!");
+        	return res.choices[0].message.content;
+      		})
+      	.catch((error) => {
+			const msg = format('createChatCompletion error : %s', error)
+			console.log(msg);
+        	// console.log(error);
+        	throw new Error(
+          	`OpenAI::createChatCompletion failed with: ${error.message}`
+        	);
+      	});
 
-    return textResponse;
-  }
+    	return textResponse;
+  	}
 
-  async getChatCompletion(messages = [], { temperature = 0.7 }) {
-    const model = process.env.OPEN_MODEL_PREF || "gpt-3.5-turbo";
-    const { data } = await this.openai.createChatCompletion({
-      model,
-      messages,
-      temperature,
-    });
+  	async getChatCompletion(messages = [], { temperature = 0.7 }) {
+		console.log("debug >>> IN : getChatCompletion")
+    	const model = process.env.OPEN_MODEL_PREF || "gpt-3.5-turbo";
+    	const { data } = await this.openai.createChatCompletion({
+      		model,
+      		messages,
+      		temperature,
+    	});
 
-    if (!data.hasOwnProperty("choices")) return null;
-    return data.choices[0].message.content;
-  }
+    	if (!data.hasOwnProperty("choices")) return null;
+    	return data.choices[0].message.content;
+  	}
 
   async embedTextInput(textInput) {
     const result = await this.embedChunks(textInput);
